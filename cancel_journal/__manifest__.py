75efd3d508492d1a32e26e44d51fd77e10db26b0
GlobalItech <30366468+GlobalItech@users.noreply.github.com>
2019-02-23 12:11:19 +0500

    Add files via upload

---
+++ cancel_journal/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name':   'Cancel Journal Entries Enterprise',
+    'author': 'Itech Resources',
+    'company': 'ItechResources',
+    'summary': 'This module is for Odoo 11 Enterprise',
+    'depends': [
+                'base',
+                'account',
+                'sale',
+                'purchase',
+                'account_cancel',
+                ],
+    
+    'data': [
+        'views/cancel.xml',
+        'security/cancel_rights.xml'
+        ,
+       
+    ],
+    
+    'application': True,
+    
+    "price": 15.00,
+    "currency": "EUR",
+}
